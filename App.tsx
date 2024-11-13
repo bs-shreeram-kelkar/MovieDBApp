6090da515b2462c1f891f12f5143b911617cf0d9
Abhishek <abhishek.patel@bombaysoftwares.com>
2024-11-13 10:40:18 +0530

    before restructure

--- App.tsx
+++ App.tsx
@@ -5,17 +5,76 @@
  * @format
  */
 
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
+  FlatList,
+  TouchableOpacity,
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons'; // Import Icon from Ionicons
+import axios from 'axios';
+import Login from './src/login'
+
+
+import {
+  Card,
+  Title,
+  Paragraph,
+  List,
+  PaperProvider,
+  Text,
+} from 'react-native-paper';
+
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+  Button,
+} from 'react-native-paper';
+import merge from 'deepmerge';
+
+
+const MyComponent = () => (
+  <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+);
+
+
+import {
+  MD2DarkTheme,
+  MD2LightTheme,
+} from 'react-native-paper';
+
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(MD3DarkTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, NavigationDefaultTheme);
 
 import {
   Colors,
@@ -29,69 +88,123 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+function HomeScreen({ navigation }): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? '#1c1c1c' : '#f5f5f5',
+  };
+
+  useEffect(() => {
+    // Fetching data from the PokeAPI using axios
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+      .then((response) => setPokemonList(response.data.results))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+  
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <View>
+      <Text>Hello, this is the Home Screen!</Text>
+
+    <Card>
+      <Card.Content>
+        <Title>title </Title>
+        <Paragraph>contnet</Paragraph>
+      </Card.Content>
+    </Card>
+      <FlatList
+        data={pokemonList}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Details', { pokemonName: item.name })}
+          >
+            <Text theme={CombinedDarkTheme} variant="displayMedium" >{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      </View>
+  );
+}
+
+function DetailsScreen({ route }) {
+  // Retrieve the parameter passed from HomeScreen
+  const { pokemonName } = route.params;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Pokémon Details</Text>
+      <Text style={styles.pokemonName}>Name: {pokemonName}</Text>
+      <MyComponent />
+    </SafeAreaView>
   );
 }
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+  <PaperProvider theme={CombinedDarkTheme}>
+    <NavigationContainer theme={CombinedDarkTheme}>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="TabScreen" component={TabScreen} options={{ headerShown: false }} />
+
+      {/* <HomeScreen /> */}
+      </Stack.Navigator>
+
+     </NavigationContainer>
+  </PaperProvider>
+  );
+}
+
+function TabScreen(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <Tab.Navigator initialRouteName="HomeTab"
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
+        if (route.name === 'HomeTab') {
+          iconName = 'home-outline';
+        } else if (route.name === 'Details') {
+          iconName = 'information-circle-outline';
+        }
+        // Return the icon component
+        return <Icon name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#2f95dc', // Active icon color
+      tabBarInactiveTintColor: 'gray',  // Inactive icon color
+      headerShown: false,
+    })}>
+    <Tab.Screen name="HomeTab" component={HomeTab} />
+    <Tab.Screen name="Details" component={SecondTab} />
+  </Tab.Navigator>
+);
+}
+
+
+function HomeTab(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+      <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+  );
+}
+
+function SecondTab(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text> Hello world</Text>
+    </View>
     </SafeAreaView>
   );
 }
@@ -113,6 +226,7 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+
 });
 
 export default App;
---
+++ src/login.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, Button, Text, useTheme } from 'react-native-paper';
+import AppDialog from './AppDialog';
+
+const Login = ({ navigation }) => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+  const { colors } = useTheme();
+  const [dialogVisible, setDialogVisible] = useState(false);
+
+  const hideDialog = () => setDialogVisible(false);
+
+
+  const handleLogin = () => {
+    // Check if the credentials match 'admin'
+    if (userId === 'admin' && password === 'admin') {
+      navigation.navigate("TabScreen");
+    } else {
+      setDialogVisible(true);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, { color: colors.primary }]}>Login</Text>
+      <TextInput
+        label="User ID"
+        value={userId}
+        onChangeText={setUserId}
+        mode="outlined"
+        style={styles.input}
+        />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        mode="outlined"
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button mode="contained" onPress={handleLogin} style={styles.button}>
+        Login
+      </Button>
+      <AppDialog
+        visible={dialogVisible}
+        onDismiss={hideDialog}
+        title="Login Failed"
+        message="Invalid User ID or Password"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  input: {
+    marginBottom: 12,
+  },
+  button: {
+    marginTop: 16,
+  },
+});
+
+export default Login;
