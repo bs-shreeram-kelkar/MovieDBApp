ca67ee2f2a4083654c4fc9548656caf7b4899b2d
Abhishek <abhishek.patel@bombaysoftwares.com>
2024-11-20 16:26:05 +0530

    fixed

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Navigation } from "react-native-navigation";
+import WelcomeScreen from "./WelcomeScreen"; // Replace with your screen
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => WelcomeScreen);
+
+Navigation.events().registerAppLaunchedListener(() => {
+   Navigation.setRoot({
+     root: {
+       stack: {
+         children: [
+           {
+             component: {
+               name: 'com.myApp.WelcomeScreen',
+             },
+           },
+         ],
+       },
+     },
+   });
+});
